f9c877ceb674a552abd0c35e605f540289a56f40
Kar4345 <kartik21gr@gmail.com>
2022-10-04 14:31:08 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const [count,setCount] = useState(1);
+  const [counterClass,setCounterClass] = useState("normal");
+
+  useEffect(()=>{
+    if(count % 3 === 0 && count % 5 === 0){
+      setCounterClass("fizzbuzz");
+    }else if(count % 3 === 0){
+      setCounterClass("fizz");
+    }else if(count % 5 === 0){
+      setCounterClass("buzz");
+    }else{
+      setCounterClass("normal");
+    }
+  },[count])
+
+
+  const onIncrementHandler=()=>{
+    setCount(count=>count + 1);
+  }
+  const onDecrementHandler = ()=>{
+    setCount(count=> count - 1);
+  }
   
   return (
     <div id="main">
-
+      <div id="counter" className={counterClass}>{count}</div>
+      <button id='increment' onClick={onIncrementHandler}>+</button>
+      <button id='decrement' onClick={onDecrementHandler}>-</button>
     </div>
   )
 }
